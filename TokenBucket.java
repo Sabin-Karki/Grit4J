d94e7b2a35fcd239c5b28dfaab8590e6946e3d8c
sabin <sabinkarki064@gmail.com>
2026-05-02 21:29:47 +0545

    added check() method

--- TokenBucket.java
+++ TokenBucket.java
@@ -1,38 +1,59 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.StampedLock;
 
 public class TokenBucket {
     private final long capacity;
     private final AtomicLong tokens;
     private final long refillRateInMillis;
     private final long refillAmount;
-    private volatile long lastRefillTime;
+    private final ScheduledExecutorService scheduler;
+    private final RefillStrategy refillStrategy;
+    private final StampedLock stampedLock;
 
-    public TokenBucket(long capacity, long refillRateInMillis, long refillAmount) {
+    public TokenBucket(long capacity, long refillRateInMillis, long refillAmount,RefillStrategy refillStrategy) {
         this.capacity = capacity;
-        this.tokens = new AtomicLong(capacity);
         this.refillRateInMillis = refillRateInMillis;
         this.refillAmount = refillAmount;
-        this.lastRefillTime = System.currentTimeMillis();
+        this.refillStrategy = refillStrategy;
+        this.tokens = new AtomicLong(capacity);
+        this.stampedLock=new StampedLock();
+
+        this.scheduler=Executors.newSingleThreadScheduledExecutor(runnable->{
+            Thread t = new Thread(runnable);
+            t.setDaemon(true);
+            return t;
+        });
+    }
+    /* the rule is 2 token per second,so the tokenToadd  = () */
+    private void refill() {
+        
+        long stamp = stampedLock.writeLock();
+        try{
+            long tokensToAdd = (refillStrategy == RefillStrategy.INTERVAL) ? refillAmount : capacity;    
+            tokens.set(Math.min(capacity,tokens.get()+tokensToAdd)); // the why is ,firstly the capacity=10 always cause its the limit,now the tokens lets assume it has 6 tokens and tokens to add is 2 so the parameter will be 8,and because of the  Math.min, the newTokenCount= will always be the newTokenCount=8 and never be more than capacity,and will be 10 at most 
+        }finally{
+            stampedLock.unlockWrite(stamp);
+        }
     }
 
     public boolean tryConsume() {
-        refill();
-        long current = tokens.get();
-        if (current >= 1)
-        return tokens.compareAndSet(current, current - 1); // current = current -1
-        return false;
-    }
 
-    /* the rule is 2 token per second,so the tokenToadd  = () */
-    private void refill() {
-        long now = System.currentTimeMillis();
-        long timePassed = now - lastRefillTime;
-        if(timePassed > refillRateInMillis){
-            long tokensToAdd = (timePassed/refillRateInMillis) * refillAmount; // how is this the formula?because
-            //for new token count ? why is Math.min tokens.get,capacity and + tokensToADd required ,what is the logic? the logic is 
-            long newTokenCount = Math.min(capacity,tokens.get()+tokensToAdd); // the why is ,firstly the capacity=10 always cause its the limit,now the tokens lets assume it has 6 tokens and tokens to add is 2 so the parameter will be 8,and because of the  Math.min, the newTokenCount= will always be the newTokenCount=8 and never be more than capacity,and will be 10 at most 
-            tokens.set(newTokenCount);
-            lastRefillTime = System.currentTimeMillis();
+        while(true){
+        long current = tokens.get();
+        if (current <= 0 ) return false;
+        if (tokens.compareAndSet(current, current - 1)) return true; //tokens is set to current-1 if the currentValue of tokens = current and it also returns true  ,else it returns false
+
         }
     }
+
+    public Decision check(){
+        boolean consumed = tryConsume();
+        if(consumed){
+            return new Decision.Allowed(tokens.get());
+        }
+        return new Decision.Throttled(refillRateInMillis);
+    }
+
 }
