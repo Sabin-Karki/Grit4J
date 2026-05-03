6d2841c171e5eff2bc5b83677d90ef90556a778a
sabin <sabinkarki064@gmail.com>
2026-05-03 12:17:04 +0545

    specific tokenbucket for each user

--- RateLimitEnforcer.java
+++ RateLimitEnforcer.java
@@ -4,13 +4,13 @@ import java.util.concurrent.ConcurrentHashMap;
 public class RateLimitEnforcer {
     private final ConcurrentHashMap<String,TokenBucket> buckets= new ConcurrentHashMap<>();
 
-    public Decision enforce(Object target,String methodName,Class<?>... paramTypes)throws NoSuchMethodException{
+    public Decision enforce(Object target,String userId,String methodName,Class<?>... paramTypes)throws NoSuchMethodException{
         Method method = target.getClass().getMethod(methodName, paramTypes);
         RateLimit annotation = method.getAnnotation(RateLimit.class);
         //if no annotation ,then user request are unlimited
         if(annotation == null) return new Decision.Allowed(Long.MAX_VALUE);
-        String key = target.getClass().getName() + "#" + methodName;
-        TokenBucket bucket = buckets.computeIfAbsent(key,k->new TokenBucket(annotation.requests(),annotation.perSeconds()*1000L,annotation.requests(),RefillStrategy.INTERVAL));
+        String key = target.getClass().getName() + "#" + methodName + userId;
+        TokenBucket bucket = buckets.computeIfAbsent(key,k->new TokenBucket(annotation.capacity(),annotation.perSeconds()*1000L,annotation.refillTokens(),RefillStrategy.INTERVAL));
         return bucket.check();
     }
 }
