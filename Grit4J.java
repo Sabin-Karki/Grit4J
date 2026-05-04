4431475ff89ff7069e8c0b4960b2a94685d63200
sabin <sabinkarki064@gmail.com>
2026-05-04 22:42:30 +0545

    main class

---
+++ Grit4J.java
@@ -0,0 +1,24 @@
+public class Grit4J {
+    @RateLimit(refillTokens = 3, perSeconds = 5)
+        public void handleRequest(String userID){
+            System.out.println("Handling request for user: " + userID);
+        }
+    
+    public static void main(String[] args) throws NoSuchMethodException,InterruptedException{
+        Grit4J service = new Grit4J();
+        RateLimitEnforcer enforcer = new RateLimitEnforcer();
+        for(int i=0;i<5;i++){
+            Decision decision = enforcer.enforce(service,"user-1","handleRequest",String.class);
+            switch (decision) {
+                case Decision.Allowed a ->
+                    System.out.println("Request " + i + "-> allowed || Tokens Left " + a.tokensRemaining() );
+                case Decision.Throttled t ->
+                     System.out.println("Request " + i + "-> throttled || Rate Limit reset in " + t.retryAfterMs());
+            }
+        }
+
+        Thread.sleep(5000);
+        System.out.println("---After Refill---");
+        
+    }
+}
