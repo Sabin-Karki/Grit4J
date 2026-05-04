561832d1eb891de6ef773ab606eff2ddf3b05286
sabin <sabinkarki064@gmail.com>
2026-05-04 23:14:10 +0545

    final additions

--- Grit4J.java
+++ Grit4J.java
@@ -1,5 +1,5 @@
 public class Grit4J {
-    @RateLimit(refillTokens = 3, perSeconds = 5)
+    @RateLimit(capacity = 5,refillTokens = 3, perSeconds = 5)
         public void handleRequest(String userID){
             System.out.println("Handling request for user: " + userID);
         }
@@ -19,6 +19,17 @@ public class Grit4J {
 
         Thread.sleep(5000);
         System.out.println("---After Refill---");
+        for(int i=0;i<5;i++){
+            Decision decision = enforcer.enforce(service,"user-1","handleRequest",String.class);
+            System.out.println("request " + i + "-> ");
+            switch(decision){
+                case Decision.Allowed a ->
+                    System.out.println("allowed || Tokens left " + a.tokensRemaining());
+                
+                case Decision.Throttled t ->
+                System.out.println("throttled || Rate limit reset in " + t.retryAfterMs());
+            }
+        }
         
     }
 }
