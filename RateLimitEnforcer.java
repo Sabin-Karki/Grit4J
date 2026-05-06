e854bb49a36fe46e68a19725359e5b3ba95c9859
sabin <sabinkarki064@gmail.com>
2026-05-06 20:20:32 +0545

    invoke method actually

--- RateLimitEnforcer.java
+++ RateLimitEnforcer.java
@@ -1,16 +1,37 @@
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class RateLimitEnforcer {
-    private final ConcurrentHashMap<String,TokenBucket> buckets= new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, TokenBucket> buckets = new ConcurrentHashMap<>();
 
-    public Decision enforce(Object target,String userId,String methodName,Class<?>... paramTypes)throws NoSuchMethodException{
+    public Decision enforce(Object target, String userId, String methodName, Object... args)
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class<?>[] paramTypes = new Class<?>[args.length];
+        for (int i = 0; i < args.length; i++) {
+            paramTypes[i] = args[i].getClass();
+        }
+
         Method method = target.getClass().getMethod(methodName, paramTypes);
         RateLimit annotation = method.getAnnotation(RateLimit.class);
-        //if no annotation ,then user request are unlimited
-        if(annotation == null) return new Decision.Allowed(Long.MAX_VALUE);
-        String key = target.getClass().getName() + "#" + methodName + userId;
-        TokenBucket bucket = buckets.computeIfAbsent(key,k->new TokenBucket(annotation.capacity(),annotation.perSeconds()*1000L,annotation.refillTokens(),RefillStrategy.INTERVAL));
-        return bucket.check();
+        if (annotation == null) {
+            method.invoke(target, args);
+            return new Decision.Allowed(Long.MAX_VALUE);
+        }
+
+        String key = target.getClass().getName() + "#" + methodName + "#" +  userId;
+        TokenBucket bucket = buckets.computeIfAbsent(
+                key,
+                k -> new TokenBucket(
+                        annotation.capacity(),
+                        annotation.perSeconds() * 1000L,
+                        annotation.refillTokens(),
+                        RefillStrategy.GREEDY));
+
+        Decision decision = bucket.check();
+        if (decision instanceof Decision.Allowed) {
+            method.invoke(target, args);
+        }
+        return decision;
     }
 }
